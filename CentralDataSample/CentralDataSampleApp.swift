d4956dbdeb986535df4026d2dd875dffea9b079a
Moji <m0ji19881122@gmail.com>
2022-04-24 01:45:56 +0900

    Initial Commit

---
+++ CentralDataSample/CentralDataSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentralDataSampleApp.swift
+//  CentralDataSample
+//
+//  Created by MacBook Air M1 on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CentralDataSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
