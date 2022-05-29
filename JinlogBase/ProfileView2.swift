1ff3287b3429f78696dbc8dbf7fcb9d5ffca64d2
yatake <73799092+yatake-a@users.noreply.github.com>
2022-05-30 07:08:31 +0900

    設定画面、プロフィール画面２を追加

---
+++ JinlogBase/ProfileView2.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView2.swift
+//  JinlogBase
+//
+//  Created by 矢竹昭博 on 2022/05/30.
+//
+
+import SwiftUI
+
+struct ProfileView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView2_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView2()
+    }
+}
