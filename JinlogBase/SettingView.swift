f3f5c8babe272c2e2dc20c001bfd0108ed619a00
yatake <73799092+yatake-a@users.noreply.github.com>
2022-05-30 07:05:03 +0900

    プロフィール画面追加

--- JinlogBase/SettingView.swift
+++ JinlogBase/SettingView.swift
@@ -1,42 +1,40 @@
 //
-//  HomeView.swift
+//  SettingView.swift
 //  JinlogBase
 //
-//  Created by 矢竹昭博 on 2022/05/28.
+//  Created by 矢竹昭博 on 2022/05/30.
 //
 
 import SwiftUI
 
-struct HomeView: View {
+struct SettingView: View {
     var body: some View {
         VStack{
             HStack{
-                Spacer()
                 //設定ボタン
                 Button(action:{
                     
                 }){
                     
-                    Image(systemName: "gearshape.fill")
+                    Text("プロフィール")
                     
                 }
             }
             Spacer()
             HStack{
                 Spacer()
-                Text("ホーム画面")
+                Text("設定画面")
                     .padding(.all, 20.0) //全ての辺に20余白
                 Spacer()
             }
             Spacer()
 
         }
-       
     }
 }
 
-struct HomeView_Previews: PreviewProvider {
+struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        SettingView()
     }
 }
