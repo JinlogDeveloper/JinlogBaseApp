8dd2488780383500e6555c961a7d9949b588ab42
yatake <73799092+yatake-a@users.noreply.github.com>
2022-05-30 06:41:00 +0900

    ホーム画面追加　HomeView.swift 歯車アイコン追加

---
+++ JinlogBase/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  JinlogBase
+//
+//  Created by 矢竹昭博 on 2022/05/28.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+
+            //設定ボタン
+            Button(action:{
+                
+            }){
+            
+            Image(systemName: "gearshape.fill")
+           
+            }
+
+
+ 
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
