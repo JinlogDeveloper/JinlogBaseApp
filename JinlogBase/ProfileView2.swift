44b4f66243d80577e3228fe03871bd37659a4b69
yatake <73799092+yatake-a@users.noreply.github.com>
2022-06-19 21:46:52 +0900

    プロフィール項目　全文登録完了

--- JinlogBase/ProfileView2.swift
+++ JinlogBase/ProfileView2.swift
@@ -37,8 +37,11 @@ struct ProfileView2: View {
                     //NavigationLinkはList内の各項目に追加する
                     NavigationLink(destination: ListNameView()) {
                         Text("ユーザーネーム")
-                    //}.badge(setting.name)
                 }.badge(ownProfile.profile.userName)
+                    
+                    NavigationLink(destination: ListBirthdayView()) {
+                        Text("誕生日")
+                    }.badge(CommonUtil.birthStr(date: ownProfile.profile.birthday, type: .yyyyMd))
                     
                     NavigationLink(destination: ListSexView()) {
                         Text("性別")
@@ -48,6 +51,19 @@ struct ProfileView2: View {
                     NavigationLink(destination: ListAreaView()) {
                         Text("都道府県")
                     }.badge(ownProfile.profile.area.name)
+                    
+                    NavigationLink(destination: ListbelongView()) {
+                        Text("所属")
+                    }.badge(ownProfile.profile.belong)
+                    
+                    
+                    
+                    NavigationLink(destination: ListIntroMessageView()) {
+                        Text("自己紹介")
+                    }.badge(ownProfile.profile.introMessage)
+                    
+                    
+                    
                     
                 }
                 
@@ -81,7 +97,7 @@ struct ProfileView2: View {
                     .font(.title)
                 
                 //入力した文字はObservableObjectで宣言した変数へ直接代入する
-                TextField("aaa",text: $bufProfile.userName)
+                TextField("ユーザーネーム",text: $bufProfile.userName)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .frame(width: 300)
                     .padding()
@@ -160,7 +176,6 @@ struct ProfileView2: View {
             }
         }
     
-    
     struct ListAreaView: View {
         private var ownProfile = OwnerProfile.sOwnerProfile
         @State private var bufProfile = Profile()
@@ -207,15 +222,147 @@ struct ProfileView2: View {
         }
     
     
+    struct ListbelongView: View {
+        @ObservedObject private var ownProfile = OwnerProfile.sOwnerProfile
+        @State private var bufProfile = Profile()
+        @State private var bufUserId: String = OwnerProfile.sOwnerProfile.userId
+
+        @Environment(\.presentationMode) var presentation
+        
+           
+        
+        var body: some View {
+            VStack(spacing: 20.0){
+                Text("所属")
+                    .font(.title)
+                
+                //入力した文字はObservableObjectで宣言した変数へ直接代入する
+                TextField("所属",text: $bufProfile.belong)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300)
+                    .padding()
+                
+                Text("ここで入力した文字はリストに表示")
+                    .multilineTextAlignment(.center)
+                    .frame(width:300)
+                
+                
+                Button(
+                    action:{
+                       ownProfile.saveProfile(uId: bufUserId, prof: bufProfile)
+                        self.presentation.wrappedValue.dismiss()
+                    }
+                ) {
+                    Text("登録")
+                        .frame(width: 180, height: 50)
+                }
+                .font(.system(size: 36))
+                .background(.yellow)
+                
+                
+                
+                
+            }
+            .onAppear {
+                bufProfile =  ownProfile.profile
+                
+            }
+        
+        }
+    }
     
+    struct ListIntroMessageView: View {
+        @ObservedObject private var ownProfile = OwnerProfile.sOwnerProfile
+        @State private var bufProfile = Profile()
+        @State private var bufUserId: String = OwnerProfile.sOwnerProfile.userId
+
+        @Environment(\.presentationMode) var presentation
+        
+           
+        
+        var body: some View {
+            VStack(spacing: 20.0){
+                Text("自己紹介")
+                    .font(.title)
+                
+                //入力した文字はObservableObjectで宣言した変数へ直接代入する
+                TextField("自己紹介",text: $bufProfile.introMessage)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300)
+                    .padding()
+                
+                Text("ここで入力した文字はリストに表示")
+                    .multilineTextAlignment(.center)
+                    .frame(width:300)
+                
+                
+                Button(
+                    action:{
+                       ownProfile.saveProfile(uId: bufUserId, prof: bufProfile)
+                        self.presentation.wrappedValue.dismiss()
+                    }
+                ) {
+                    Text("登録")
+                        .frame(width: 180, height: 50)
+                }
+                .font(.system(size: 36))
+                .background(.yellow)
+                
+                
+                
+                
+            }
+            .onAppear {
+                bufProfile =  ownProfile.profile
+                
+            }
+        
+        }
+    }
     
-    
-    
-    
+    struct ListBirthdayView: View {
+        private var ownProfile = OwnerProfile.sOwnerProfile
+        @State private var bufProfile = Profile()
+        @State private var bufUserId: String = OwnerProfile.sOwnerProfile.userId
+        
+        @Environment(\.presentationMode) var presentation
+        
+        var body: some View {
+            
+            VStack(spacing:50){
+                Text("誕生日を選択")
+                    .font(.title)
+                
+                //Pickerで選択
+                //選択した数字はObservableObjectで宣言した変数へ直接代入する
+                DatePicker("誕生日を選択",selection: $bufProfile.birthday, displayedComponents: .date)
+                //Pickerのデザインはホイールにしたかったのでスタイルを指定
+                .pickerStyle(WheelPickerStyle())
+                .onAppear {
+                    bufProfile =  ownProfile.profile
+                }
+
+                
+                Button(
+                    action:{
+      
+                       ownProfile.saveProfile(uId: bufUserId, prof: bufProfile)
+                        self.presentation.wrappedValue.dismiss()
+                        
+                    }
+                ) {
+                    Text("登録")
+                        .frame(width: 180, height: 50)
+                }
+                .font(.system(size: 36))
+                .background(.yellow)
+                
+            }
+        }
     
     
     }
-    
+}
 
 struct ProfileView2_Previews: PreviewProvider {
     static var previews: some View {
