99f57e567da51f17a422a697a38c13494a519e68
yatake <73799092+yatake-a@users.noreply.github.com>
2023-01-21 06:38:49 +0900

    なぜか、gitの管理対象から外れてしまったので、追加 QRCodeCameraDelegate.swift QRCodeScannerView.swift

--- JinlogBase/View/MainView/GameView/QRCodeCameraDelegate.swift
+++ JinlogBase/View/MainView/GameView/QRCodeCameraDelegate.swift
@@ -9,12 +9,9 @@ import AVFoundation
 
 class QrCodeCameraDelegate: NSObject, AVCaptureMetadataOutputObjectsDelegate {
     
-    var scanInterval: Double = 1.0
     var lastTime = Date(timeIntervalSince1970: 0)
     
     var onResult: (String) -> Void = { _  in }
-    // ★なくしてOK！
-    var mockData: String?
     
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         if let metadataObject = metadataObjects.first {
@@ -23,21 +20,11 @@ class QrCodeCameraDelegate: NSObject, AVCaptureMetadataOutputObjectsDelegate {
             foundBarcode(stringValue)
         }
     }
-    // ★なくしてOK！
-    @objc func onSimulateScanning(){
-        foundBarcode(mockData ?? "Simulated QR-code result.")
-    }
+    
     
     func foundBarcode(_ stringValue: String) {
-        // ★（疑問）
-        // ★この部分の処理は、QRコードが見つかっている間、超連続で呼ばれ続けてて、
-        let now = Date()
-        if now.timeIntervalSince(lastTime) >= scanInterval {
-            // ★インターバル以上毎に走る処理は、文字列を反映するこの処理だけ！？
-            lastTime = now
-            self.onResult(stringValue)
-            // ★もしそうであれば、インターバル以上毎に反映するのではなく、
-            // ★読み取った文字列が変更される毎に反映するようにした方がよさそう
-        }
+        
+        self.onResult(stringValue)
+        
     }
 }
